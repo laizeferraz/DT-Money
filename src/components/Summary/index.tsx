e5e32ed13cae9fbfb6273a7b968326df8f6ef353
Laize Ferraz <laizeferraz@gmail.com>
2023-08-04 18:42:14 +1000

    create summary cards

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,35 @@
+import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react'
+import { SummaryCard, SummaryContainer } from './styles'
+
+export function Summary() {
+  return (
+    <SummaryContainer>
+      <SummaryCard>
+        <header>
+          <span>Income</span>
+          <ArrowCircleUp size={32} color="#00b37e" />
+        </header>
+
+        <strong></strong>
+      </SummaryCard>
+
+      <SummaryCard>
+        <header>
+          <span>Expenses</span>
+          <ArrowCircleDown size={32} color="#f75a68" />
+        </header>
+
+        <strong></strong>
+      </SummaryCard>
+
+      <SummaryCard variant="green">
+        <header>
+          <span>Total</span>
+          <CurrencyDollar size={32} color="#fff" />
+        </header>
+
+        <strong></strong>
+      </SummaryCard>
+    </SummaryContainer>
+  )
+}
